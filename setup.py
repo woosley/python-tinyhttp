51022489df1c31b7dafa1e0f380625779b8488dd
woosley.xu <woosley.xu@gmail.com>
2016-09-22 22:05:09 +0800

    add setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = "tinyhttp",
+    version = '0.0.1',
+    packages = find_packages(),
+    author = 'woosley.xu',
+    author_email = "woosley.xu@gmail.com",
+    description = "a tiny http client implementation",
+    license = "BSD",
+)
