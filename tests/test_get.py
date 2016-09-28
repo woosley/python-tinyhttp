a64b6d18e78edd7c05cf7bb2173db9c7f59847a2
woosley.xu <woosley.xu@gmail.com>
2016-09-28 22:03:43 +0800

    add a new test for chunked body

--- tests/test_get.py
+++ tests/test_get.py
@@ -8,6 +8,14 @@ def test_get_with_params(http, httpbin):
     res = http.get(httpbin.url + "/get?params=1&params=2")
     assert res['status'] == '200'
 
+# builtin httpserver in Werkzeug does not support chunked data
+# httpbin.org supports, see
+# https://github.com/Runscope/httpbin/issues/301 for more details
 def test_streamed_data(http, httpbin):
     res = http.get(httpbin.url + "/stream/10")
+    assert res['status'] == '200'
+
+def test_chunked_data(http):
+    url = "http://httbin.org/stream/10"
+    res = http.get(url)
     assert res['status'] == '200'
